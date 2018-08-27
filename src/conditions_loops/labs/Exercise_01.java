468097df6bcaa5e02c1c5f3723138593f128e278
Tom André Hansen <tom@ing.vin>
2018-08-27 20:57:17 +0200

    started looking at loop - did exercise 01 in conditions and loops

--- src/conditions_loops/labs/Exercise_01.java
+++ src/conditions_loops/labs/Exercise_01.java
@@ -19,11 +19,42 @@ public class Exercise_01 {
         // create scanner
         Scanner scanner = new Scanner(System.in);
         // prompt user
-        System.out.print("Enter a number between 1 and 1,000,000,000 to determine whether it is odd or even: ");
+        System.out.print("Enter a number between 1 and 1,000,000 to determine whether it is odd or even (-7 to exit): ");
         // assign input to variable as int
         int number = scanner.nextInt();
 
-        // write completed code here
+        do {
+            if(number < 0){
+                System.out.println(number + " is too low");
 
+            } else if (number > 1000000){
+                System.out.println(number + " is too high");
+
+            } else {
+                if(number % 2 == 0){
+                    System.out.println(number + " is even");
+                } else {
+                    System.out.println(number + " is odd");
+                }
+            }
+
+            System.out.print("Enter a number between 1 and 1,000,000 to determine whether it is odd or even (-7 to exit): ");
+
+            number = scanner.nextInt();
+
+        } while(number != -7);
+        System.out.println("Thanks for using the even/odd number checker. Goodbye!");
+
+        int val = 0;
+
+        System.out.println("while loop");
+        while (val < 10){
+            System.out.println(val);
+            val++;
+        }
+        System.out.println("for loop");
+        for (int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
