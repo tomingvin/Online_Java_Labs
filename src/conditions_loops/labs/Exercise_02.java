61e8bcb53c8dd3be142dc946d4753e24f4410e83
Tom André Hansen <tom@ing.vin>
2018-08-29 20:49:49 +0200

    conditions, exercise 02

--- src/conditions_loops/labs/Exercise_02.java
+++ src/conditions_loops/labs/Exercise_02.java
@@ -15,13 +15,37 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // create scanner
-
+        Scanner scanner = new Scanner(System.in);
         // prompt user
-
+        System.out.print("Tell me a number, and I'll tell you what day in a week has that number: ");
         // assign input to variable as int
+        int number = scanner.nextInt();
+
+            if (number == 1) {
+                System.out.println("Monday");
+
+            } else if (number == 2) {
+                System.out.println("Tuesday");
+
+            } else if (number == 3) {
+                System.out.println("Wednensday");
+
+            } else if (number == 4) {
+                System.out.println("Thursday");
+
+            } else if (number == 5) {
+                System.out.println("Friday");
+
+            } else if (number == 6) {
+                System.out.println("Saturday");
+
+            } else if (number == 7) {
+                System.out.println("Sunday");
 
 
-        // write completed code here
+            } else {
+                System.out.println("There are 7 days in a week, so " + number + " is not an option");
+            }
 
+        }
     }
-}
