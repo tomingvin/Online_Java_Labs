5777c840b44cea7dfdaa7abc74873e330b1534bb
Tom André Hansen <tom.andre.hansen@maxmanus.com>
2018-08-30 13:38:03 +0200

    completed condition Exercise 05

--- src/conditions_loops/labs/Exercise_05.java
+++ src/conditions_loops/labs/Exercise_05.java
@@ -1,5 +1,7 @@
 package conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 5: Calculator
  *
@@ -16,4 +18,43 @@ package conditions_loops.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+
+        //input from user, a lover and upper number
+        System.out.println("I will calculate the value of some numbers, and their average. Please give me a lower and upper number :) Press enter to continue...");try{
+            System.in.read();}catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        // create scanner
+        Scanner lowerScan = new Scanner(System.in);
+        // prompt user
+        System.out.print("Lower: ");
+        // assign input to variable as int
+        int lower = lowerScan.nextInt();
+
+        // create scanner
+        Scanner upperScan = new Scanner(System.in);
+        // prompt user
+        System.out.print("Upper: ");
+        // assign input to variable as int
+        int upper = upperScan.nextInt();
+
+        int sum = 0;
+        double average = 0.0;
+
+        //create a for-loop, for use in calculations below
+        for(int averageLoop = lower; averageLoop <= upper; averageLoop++ ){
+          sum = sum + averageLoop;
+          average = (double)sum / (upper - lower + 1);
+        }
+
+        //sum of numbers
+
+        System.out.println("The sum of all the numbers is: " + sum);
+
+        //average of the numbers
+
+        System.out.println("The average value of all the numbers is: " + average);
+    }
 }
