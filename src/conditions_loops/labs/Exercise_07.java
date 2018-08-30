e6d1ca1f2bdb2e3357ffadc6e22747538d4363d0
Tom André Hansen <tom@ing.vin>
2018-08-30 21:13:52 +0200

    started on cond exer 07 - figured out scanner and strings

--- src/conditions_loops/labs/Exercise_07.java
+++ src/conditions_loops/labs/Exercise_07.java
@@ -1,5 +1,7 @@
 package conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 7: First vowel
  *
@@ -9,4 +11,14 @@ package conditions_loops.labs;
  */
 
 public class Exercise_07 {
+    public static void main(String[] args) {
+
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        // prompt user
+        System.out.print("Some text: ");
+        // assign input to variable as int
+        String vowel = scanner.nextLine();
+        System.out.println(vowel);
+    }
 }
