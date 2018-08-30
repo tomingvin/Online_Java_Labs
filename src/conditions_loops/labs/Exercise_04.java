aabed6c5550f59f2f57a675caa861735ef2addf9
Tom André Hansen <tom.andre.hansen@maxmanus.com>
2018-08-30 09:52:25 +0200

    small correction to Exercise 04, added D'OH for odd numbers

--- src/conditions_loops/labs/Exercise_04.java
+++ src/conditions_loops/labs/Exercise_04.java
@@ -14,5 +14,7 @@ public class Exercise_04 {
         for(evenNumbers = 1; evenNumbers <= 100; evenNumbers++)
             if(evenNumbers % 2 == 0)
             System.out.println(evenNumbers);
+            else System.out.println("D'OH!");
+
     }
 }
