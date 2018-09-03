45f61232bec102648baf70169330abbaa16d9c0e
Tom André Hansen <tom.andre.hansen@maxmanus.com>
2018-09-03 14:08:29 +0200

    completed exer 09

--- src/conditions_loops/labs/Exercise_09.java
+++ src/conditions_loops/labs/Exercise_09.java
@@ -8,4 +8,16 @@ package conditions_loops.labs;
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+
+        for (int number = 0; number <= 10; number++)
+        {
+            if (number==5)
+            {
+                break;
+            }
+
+            System.out.print(number + " ");
+        }
+    }
 }
