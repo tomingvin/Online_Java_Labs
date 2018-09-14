df30cb918893f79ba1dbd9046d1288b0e68a28d3
Tom André Hansen <tom.andre.hansen@maxmanus.com>
2018-09-14 15:06:06 +0200

    working on methods

--- src/objects_classes_methods/labs/Exercise_02.java
+++ src/objects_classes_methods/labs/Exercise_02.java
@@ -12,37 +12,67 @@ package objects_classes_methods.labs;
  */
 
 
-class Fruit {
-    public static void main(String[] args) {
-
-        Fruit apple = new Fruit();
-        Fruit pear = new Fruit();
-        System.out.println(apple.toString());
-        System.out.println(pear.toString());
-
-
-
-    }
-
-    public static void staticSecond(){
-
-        int number;
-        int age;
-
+class Test
+{
+    public static int i = 0;
+    // constructor of class which counts
+    //the number of the objects of the class.
+    Test()
+    {
+        i++;
 
     }
-
-    static void nunmberOfEatable(int eatable){
-
-        //totalt number minus isItRotten
-        int eatable
-
+    // static method is used to access static members of the class
+    // and for getting total no of objects
+    // of the same class created so far
+    public static int get()
+    {
+        // statements to be executed....
+        return i;
     }
 
-    static void isItRotten(){
+    // Instance method calling object directly
+    // that is created inside another class 'GFG'.
+    // Can also be called by object directly created in the same class
+    // and from another method defined in the same class
+    // and return integer value as return type is int.
+    public int m1()
+    {
+        System.out.println("Inside the method m1 by object of GFG class");
 
-        //if older than 12 months, then rotten
+        // calling m2() method within the same class.
+        this.m2();
 
+        // statements to be executed if any
+        return 1;
     }
 
+    // It doesn't return anything as
+    // return type is 'void'.
+    public void m2()
+    {
+
+        System.out.println("In method m2 came from method m1");
+    }
+}
+
+class GFG
+{
+    public static void main(String[] args)
+    {
+        // Creating an instance of the class
+        Test obj = new Test();
+
+        // Calling the m1() method by the object created in above step.
+        int i = obj.m1();
+        System.out.println("Control returned after method m1 :" + i);
+
+        // Call m2() method
+        // obj.m2();
+        int no_of_objects = Test.get();
+
+        System.out.print("No of instances created till now : ");
+        System.out.println(no_of_objects);
+
+    }
 }
