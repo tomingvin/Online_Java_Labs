c37dc2642ec313ede656be0ec8f0b045287d25e9
Tom André Hansen <tom.andre.hansen@maxmanus.com>
2018-09-16 21:38:30 +0200

    working on methods

--- src/objects_classes_methods/labs/Exercise_02.java
+++ src/objects_classes_methods/labs/Exercise_02.java
@@ -12,67 +12,23 @@ package objects_classes_methods.labs;
  */
 
 
-class Test
-{
-    public static int i = 0;
-    // constructor of class which counts
-    //the number of the objects of the class.
-    Test()
-    {
-        i++;
+class main {
+
+    public static int first (int a, int b) {
+
+        int sum = a + b;
+        System.out.println(sum);
+        return(sum);
 
     }
-    // static method is used to access static members of the class
-    // and for getting total no of objects
-    // of the same class created so far
-    public static int get()
-    {
-        // statements to be executed....
-        return i;
-    }
+    public static void second(int something) {
 
-    // Instance method calling object directly
-    // that is created inside another class 'GFG'.
-    // Can also be called by object directly created in the same class
-    // and from another method defined in the same class
-    // and return integer value as return type is int.
-    public int m1()
-    {
-        System.out.println("Inside the method m1 by object of GFG class");
+        something = first(1, 2);
 
-        // calling m2() method within the same class.
-        this.m2();
-
-        // statements to be executed if any
-        return 1;
-    }
-
-    // It doesn't return anything as
-    // return type is 'void'.
-    public void m2()
-    {
-
-        System.out.println("In method m2 came from method m1");
-    }
 }
 
-class GFG
-{
-    public static void main(String[] args)
-    {
-        // Creating an instance of the class
-        Test obj = new Test();
 
-        // Calling the m1() method by the object created in above step.
-        int i = obj.m1();
-        System.out.println("Control returned after method m1 :" + i);
-
-        // Call m2() method
-        // obj.m2();
-        int no_of_objects = Test.get();
-
-        System.out.print("No of instances created till now : ");
-        System.out.println(no_of_objects);
-
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
